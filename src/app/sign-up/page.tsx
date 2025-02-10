c7f69a9042440eaaf39e5c845140df61917586a6
VigBon <ckube2033@gmail.com>
2025-02-10 16:30:31 +0700

    create: Sign-Up Page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -20,19 +20,30 @@ import { Input } from "@/components/ui/input";
 import { Card } from "@/components/ui/card";
 import { Eye, EyeClosed } from "lucide-react";
 
-export default function Login() {
+export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   //Show/Hide password
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
   };
 
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword((prevState) => !prevState);
+  };
+
   //Object check
-  const formSchema = z.object({
-    email: z.string().email(),
-    password: z.string(),
-  });
+  const formSchema = z
+    .object({
+      email: z.string().email(),
+      password: z.string(),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Password is incorrect",
+      path: ["confirmPassword"],
+    });
 
   //Define your form
   const form = useForm<z.infer<typeof formSchema>>({
@@ -40,6 +51,7 @@ export default function Login() {
     defaultValues: {
       email: "",
       password: "",
+      confirmPassword: "",
     },
   });
   //Define a submit handler
@@ -50,22 +62,20 @@ export default function Login() {
     <div className="min-h-screen bg-white flex items-center justify-center flex-wrap">
       <div className="w-1/2 h-auto flex justify-center pb-10">Logo</div>
       <div className="w-1/2 h-auto">
-        <Card className=" max-w-sm w-full bg-white border-0">
+        <Card className=" max-w-sm w-full bg-white border-0 shadow-none">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
               <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
                   <FormItem>
                     <div className="relative">
-                      <FormLabel>
-                        Email
-                      </FormLabel>
+                      <FormLabel>Email</FormLabel>
                       <FormControl>
                         <Input
                           {...field}
-                          className="w-full bg-white  text-white border border-gray-700 focus:outline-none focus:ring focus:ring-orange-500 py-5"
+                          className="w-full bg-white  text-black border border-gray-700 focus:outline-none focus:ring focus:ring-orange-500 py-5"
                           name="email"
                           autoComplete="off"
                         />
@@ -81,9 +91,7 @@ export default function Login() {
                 render={({ field }) => (
                   <FormItem>
                     <div className="relative">
-                      <FormLabel>
-                        Password
-                      </FormLabel>
+                      <FormLabel>Password</FormLabel>
                       <FormControl>
                         <Input
                           {...field}
@@ -104,29 +112,48 @@ export default function Login() {
                   </FormItem>
                 )}
               />
-              <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-2 hover:opacity-85">
-                  <Button className="p-0" variant={"link"}>
-                    <Link href={"/sign-up"} className="text-sm">
-                      Create New Account
-                    </Link>
-                  </Button>
-                </div>
-                <Link
-                  href={"/"}
-                  className="text-sm text-black underline hover:opacity-85"
-                >
-                  {" "}
-                  Forgot password{" "}
-                </Link>
-              </div>
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="relative">
+                      <FormLabel>Confirm Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="w-full bg-white text-black border border-gray-700 focus:outline-none focus:ring focus:ring-orange-500 py-5"
+                          type={showConfirmPassword ? "text" : "password"}
+                          name="pass"
+                          autoComplete="off"
+                        />
+                      </FormControl>
+                      <span
+                        onClick={toggleConfirmPasswordVisibility}
+                        className="absolute right-2 top-[90%] translate-y-[-100%] cursor-pointer text-black"
+                      >
+                        {showConfirmPassword ? <Eye /> : <EyeClosed />}
+                      </span>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
               <Button
                 className=" w-full bg-black hover:bg-opacity-80 text-white text-xl py-5"
                 type="submit"
               >
-                Login
+                Sign Up
               </Button>
+
+              <div className="flex items-center justify-center">
+                <Button className="p-0" variant={"link"}>
+                  <Link href={"/login"} className="text-sm">
+                    I have a account
+                  </Link>
+                </Button>
+              </div>
             </form>
           </Form>
         </Card>
