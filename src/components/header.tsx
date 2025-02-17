c7eb23d3672732bfb4b90b9a6ca7bdb9dbb1d731
haiyen0802 <joncer.609@gmail.com>
2025-02-17 16:12:57 +0700

    fix ui bug login contact page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,18 +14,16 @@ export default function Header() {
   const pathname = usePathname();
   const isHome = pathname === "/" || pathname ==="/Home";
   return (
-    <header className="fixed w-full top-0 z-10 flex justify-center px-8 bg-white">
-      {`w-full z-10 flex justify-center px-8 bg-white ${isHome ? "fixed top-0" : "relative"}`}
-
+    <header className={`w-full z-10 flex justify-center px-8 bg-white ${isHome ? "fixed top-0" : "relative"}`}>
       <div className="container flex justify-between items-center py-2 border-b border-gray-500 backdrop-blur ">
         <Link className="text-lg font-bold md:text-xl" href={"/"}>
           AIdo.
         </Link>
         <ul className="hidden lg:flex w-full justify-end items-center space-x-12">
           <li><Link href={"/"}>Home</Link></li>
-          <li><Link href={"/"}>About</Link></li>
+          <li><Link href={"/about"}>About</Link></li>
           <li><Link href={"/blog"}>Blog</Link></li>
-          <li><Link href={"/"}>Technology Page</Link></li>
+          <li><Link href={"/contact"}>Contact</Link></li>
           <li>
             <Button>
               <Link href={"/"}>Reach out to us</Link>
