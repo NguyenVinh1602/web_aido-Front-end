f9ed84d14920ef2df7f83ad31dd3ce1646a76dc5
haiyen0802 <joncer.609@gmail.com>
2025-02-17 15:47:53 +0700

    update blog page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import OpeningStatement from "../components/openingStatement";
 import Home from "./home/page";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 export default function Landing() {
   return (
     <div>
